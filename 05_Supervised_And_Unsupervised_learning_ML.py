e41e815aa29b206649990840f2ee39adc4b1e0e4
Tayyab Malik <mtayyabmalik99@gmail.com>
2021-08-23 15:43:01 +0500

    1.0

---
+++ 05_Supervised_And_Unsupervised_learning_ML.py
@@ -0,0 +1,28 @@
+# (05)*********************Supervised and unsupervised learning in Machine Learning*****************
+
+# ----How will a Machine learn?
+# ----ML Algorithms----ML with Problem Understanding!----In Machine Learning ,data and techniques is important but equally important the type of problem(Supervised/Unsupervised)
+
+# ----Steps in a Typical Machine Learning Problems
+# --(1)-Problem Identification
+# --(2)-Data Collection and Validation
+# --(3)-Model Building
+# --(4)-Feedback
+
+# ----What is Supervised Learning?----Machine knows something beforehand
+# ----Supervised Learning---Teaching the Machine
+# --(1)-Already Tagged Data
+# --(2)-Features and Labels present
+# --(3)-Classification---Calssify which label a given set of features belog to!
+# --(4)-Regression-------Find out the value of the label using previous data!
+
+# ----What is Unsupervised Learing?----All machine knows is the data in front of it. No Features! No Labels
+# ---Unsupervised Learning---Teaching the machine
+# --(1)-Data Not Already Tagged
+# --(2)-Features and labels not present
+# --(3)-Training not done
+# --(4)-Clustering and Association
+# ----Clustering----Discover the inherent grouping in the data, such as grouping customers by purchasing behaviour.
+# ----Association---Association rule learing problem. such as people that buy X also tend to buy Y
+
+

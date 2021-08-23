a66c84918a3071d081cbb778e54bdc2f8c883695
Tayyab Malik <mtayyabmalik99@gmail.com>
2021-08-23 17:11:50 +0500

    1.0

---
+++ 07_Training_And_Test_Data_ML.py
@@ -0,0 +1,5 @@
+# (07)***********************Training and Test Data in Machine Learning*************************
+
+# -----first of all we take some values and assign the algorithm 
+# -----then we take 60 to 70% data appling as a test if the test data is returning correct values then our algorithm is correct and accurate if returning values are not correct then the algorithm is not correct and accurate
+# ----thats why we categories the data in to two parts------train and test data

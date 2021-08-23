7b5cb7d394506bf88314629fd8624321ff635d17
Tayyab Malik <mtayyabmalik99@gmail.com>
2021-08-23 18:56:33 +0500

    1.0

--- 09_Multivariable_Linear_Regression_ML.py
+++ 09_Multivariable_Linear_Regression_ML.py
@@ -1,4 +1,4 @@
-# (08)***********************Simple Linear Regression in Machine Learning**************************
+# (09)***********************Multivariable Linear Regression in Machine Learning**************************
 
 # -----Definition of Linear Regression---Linear Regression is a supervised machine learning algorithm where the predicted output is continuous and has a constant slope. It's used to predict values within a continuous range, (e.g. sales, price) rather than trying to classify them into categories (e.g. cat, dog). There are two main types:
 # ----(1)-Simple Linear Regression

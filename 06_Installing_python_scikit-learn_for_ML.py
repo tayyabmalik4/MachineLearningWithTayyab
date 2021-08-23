119ad7d0e107e0ced3b9994d362eaa097c33b136
Tayyab Malik <mtayyabmalik99@gmail.com>
2021-08-23 16:24:05 +0500

    1.0

---
+++ 06_Installing_python_scikit-learn_for_ML.py
@@ -0,0 +1,17 @@
+# (06)***********************Installing python scikit-learn for Machine Learning****************
+
+# ---Scikit learn is a python library--
+# ---Scikit learn introduced and made by Google----also open source
+
+# ---Using Sklearn for Machine Learning
+# --(1)-Simple and efficient tools for data mining and data analysis
+# --(2)-Accessible to everybody, and reusable in various contexts
+# --(3)-Build on Numpy,SciPy, and Matplotlib
+# --(4)-Open Source, commercially usable-BSD license
+
+# ----How to download sklearn?
+# ----pip install sklearn
+# ----show the version
+import sklearn as skl
+version=skl.__version__
+print(version)

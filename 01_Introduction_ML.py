9f4dd896f8987c53ac544396c3d2f7bf3e6f444f
Tayyab Malik <mtayyabmalik99@gmail.com>
2021-08-23 13:11:36 +0500

    1.0

---
+++ 01_Introduction_ML.py
@@ -0,0 +1,18 @@
+# (01)*************************Introduction of Machine Learning*******************************
+
+# ----Discuss about---Whay learn Machine learning 
+# ----we teach the program which program learn it and behave like a human
+
+# ----Why we learn Machine Learning
+# --(1)-Better Career Opportynities
+# --(2)-High Salaries
+# --(3)-ML is the Future
+# --(4)-ML Products are Trending in the market
+# --(5)-Highly Demanded
+# --(6)-ML is FUN
+
+# ----Why we use python in Machine Learning
+# --(Python is Easy)
+# --(Python is fast Enough)
+# --(Python has ML Libraries)
+# --(Python is Cross Platform)

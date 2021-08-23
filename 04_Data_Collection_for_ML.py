0b96923378cd523e4828fe07a63a35caefebbab8
Tayyab Malik <mtayyabmalik99@gmail.com>
2021-08-23 15:16:25 +0500

    1.0

---
+++ 04_Data_Collection_for_ML.py
@@ -0,0 +1,10 @@
+# (04)***********************Data Collection for Machine Learning*********************************
+
+# ----Datasets are Important?----Feeding data and instructing machine on what to do and how to behave is the most important aspects of Machine Learning.
+
+# ----Where we collect the data for Machine Learning?
+# /// we can get data from-----
+# ---(1)-From your company
+# ---(2)-From an Investor willing to trust you on his data
+# ---(3)-From online data scraping
+# ---(4)-From UCI ML repository! What is it?-----https://archive.ics.uci.edu/ml/index.php

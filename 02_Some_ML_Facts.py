1704bfed5107ad568c9f750918bd72ca52a852bb
Tayyab Malik <mtayyabmalik99@gmail.com>
2021-08-23 14:49:58 +0500

    1.0

---
+++ 02_Some_ML_Facts.py
@@ -0,0 +1,9 @@
+# (02)************************Some Machine Learning Facts******************************
+
+# ---Machine learning is very trending  
+
+# ----AlphaGo----playing game----
+# ----DeepMind----purchases Google
+# ----AutoDrive(Tesla)
+# ----SelfPlay Game
+# ----
---
+++ 03_Features_and_Labels_in_ML.py
@@ -0,0 +1,4 @@
+# (03)********************Features and Labels in ML*******************
+
+# -----When we train a program than program take many instructions and Features and Labels then Program Decide that Who is the Category Falling
+# -----Example----self-driving car, auto play game, Youtube recommendations, google search, face detection

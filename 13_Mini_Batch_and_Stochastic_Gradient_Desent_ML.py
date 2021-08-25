7fe4ebe9f51dcd0cf43febe7475333d50f73e3b2
Tayyab Malik <mtayyabmalik99@gmail.com>
2021-08-25 22:39:40 +0500

    1.0

---
+++ 13_Mini_Batch_and_Stochastic_Gradient_Desent_ML.py
@@ -0,0 +1,9 @@
+# (13)*******************************Mini Batch and Stochastic Gradient Desent in Machine Learning***************************
+
+# ----Root----features----Model(Prediction function)--->>>Compute Loss---->>>Update Parameters-->>>---Again--Model(Predicted function)---
+# ------------Labels--------------------------------->>>>^^^^^^^^^^^^^
+
+# -----Batch-----The lenght of the rows in dataset
+# -----Gradient Desent-----full lenght of the Batch in datasets
+# -----Mini Batch Gradient Decent----As we choose the Batch -----for example----we choose the 100 baches out of 1M batches in dataset
+# -----Stochastic Gradient Desent----One Batch of the datasets----for example----one batch out of 1M.  

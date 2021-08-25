c5a8f486d8874dce2da0a9801878bfa1a52ef630
Tayyab Malik <mtayyabmalik99@gmail.com>
2021-08-25 21:57:02 +0500

    1.0

---
+++ 12_Loss_Functions_And_Gradient_Desent_ML.py
@@ -0,0 +1,26 @@
+# (12)**********************************Loss Function and Gradient Desent in Machine Learning*****************************
+
+
+# ----Loss function difinition----Loss functions measure how far an estimated value is from its true value. A loss function maps decisions to their associated costs. Loss functions are not fixed, they change depending on the task in hand and the goal to be met
+
+# ----Gradient Desent Definition----Gradient Descent is an optimization algorithm for finding a local minimum of a differentiable function. Gradient descent is simply used in machine learning to find the values of a function's parameters (coefficients) that minimize a cost function as far as possible.
+
+# ----Why are the important Loss Function And Gradient Decent in Machine Learnu=ing Algorithm?
+# ----Well the ML Algorithm Works on the instructions of the loss function.
+
+# --(1)-Exact Methods computationally expensive.
+# --(2)-Loss function give direction of Optimal solution
+# --(3)-Fast enough to scale on big data
+# --(4)-Easy to understand
+
+# ----Mean Absolute Error/L1 Loss-----Formula-----MAE=Σ|Yi - Y'i|/n
+# ----In the internet we observe this formula-----MAE = (1/n) * Σ|yi – xi|
+
+# ----Other Loss functions?
+# --(1)-Cross-Entropy
+# --(2)-Hinge
+# --(3)-Huber
+# --(4)-Kullback-Leibler
+# --(5)-MAE(L1)
+# --(6)-MSE(L2)
+

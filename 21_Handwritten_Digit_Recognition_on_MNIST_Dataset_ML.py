40f1b64d885ef027e4f1711a63b333bffdf29348
Tayyab Malik <mtayyabmalik99@gmail.com>
2021-09-01 21:37:36 +0500

    1.0

---
+++ 21_Handwritten_Digit_Recognition_on_MNIST_Dataset_ML.py
@@ -0,0 +1,5 @@
+# (21)*****************************Handwritten Digit Recognition on MNIST Dataset in Machine Learning***********************
+
+# ******What is MNIST?
+# ---Set of 70000 small images of digits handwritten by high school students and employees of the US Census Bureau
+# ---Set of 70000 small images of digits handwritten by high school students and employees of the US Census Bureau
